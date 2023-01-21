d80859c7a67db7b18775992d03cb82f54e9a22b2
William Milestone <wmilestone@gmail.com>
2023-01-21 16:22:37 -0600

    feat: Functional Predictor

--- Functional_Call_Predictors.py
+++ Functional_Call_Predictors.py
@@ -2,7 +2,18 @@ import numpy as np
 import numpy.linalg as la
 import pandas as pd
 import matplotlib as plt
+from functionsFFDataMan import *
+#Pull database info database=original database for ref, pros=prospect database after cleansing
+POS='WR'
+database=pd.read_excel("New"+POS+"Prospects.xlsx")
+
+#Call database cleaning
+pros=DataCleans(database,0)
+
+t_columns='AVG PPG YR 1-3'
+training_columns=['DR','AVGREC Yards/TM PA','AVGTouch/Team Attempt']
 
 
-database=pd.read_excel("QBProspects.xlsx")
-seas=pd.read_excel("QBProspects.xlsx")
+output=pred(pros,training_columns,t_columns,2021)
+
+print('wait')
--- functionsFFDataMan.py
+++ functionsFFDataMan.py
@@ -1,8 +1,12 @@
+import numpy as np
+import numpy.linalg as la
+import pandas as pd
+import matplotlib as plt
 def DataCleans(df,P5_only_on):
 
     df.loc[(df['DR']=='UDFA'),'DR']=8
     df.loc[(df['DP']=='UDFA'),'DP']=260
-
+    df=df.replace('-',np.nan)
     Pow5=['Big 12','ACC','SEC','Pac-12','Big Ten']
 
     if P5_only_on==1:
@@ -12,3 +16,38 @@ def DataCleans(df,P5_only_on):
 
 
     return df
+
+
+
+
+def pred(df, train,targ,predictor_year):
+    dfstore=df
+    df=df.dropna(subset=[targ])
+    df=df.dropna(subset=train)
+
+    seas=df.reset_index()
+    targ_vals=seas[targ].to_frame()
+
+    xarg_vals=seas[train]
+    col_o_1s=pd.DataFrame(index=np.arange(len(xarg_vals)),columns=['1cols'])
+    col_o_1s.loc[:,:]=1
+
+    xarg_vals['1col']=col_o_1s
+    xarg_vals=xarg_vals.apply(pd.to_numeric, errors='coerce')
+    xarg_vals=xarg_vals.to_numpy()
+
+    w=np.dot(la.pinv(xarg_vals),targ_vals)
+
+    #Now pull rookies 
+    rooks=dfstore
+    rooks=rooks.loc[(rooks['Draft Year']==predictor_year) & (rooks['DR']!='UDFA')]
+    rooksvals=rooks[train]
+    rooksvals['1col']=col_o_1s
+    
+    rookpred=np.dot(rooksvals,w)
+
+    name_store=rooks['Player']
+    predicted=pd.DataFrame(pd.np.column_stack([name_store,rookpred]))
+
+    return predicted
+
